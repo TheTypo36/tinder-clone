8c4b45daec69652ce8f0f8421e2bc56950b1b2d0
Ashish Raghuvanshi <thetypo36@gmail.com>
2022-08-24 13:12:35 +0530

    add a header component and also material ui to the project

---
+++ tinder-clone-client/src/components/App.js
@@ -0,0 +1,15 @@
+import "../styles/App.css";
+import Header from "./Header.js";
+function App() {
+  return (
+    <div className="App">
+      Tinder Clone
+      {/* Header */}
+      <Header />
+      {/* TinderCard */}
+      {/* Buttons */}
+    </div>
+  );
+}
+
+export default App;
---
+++ tinder-clone-client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+
+function Header() {
+  return (
+    <div className="Header">
+      <AccountBoxIcon />
+    </div>
+  );
+}
+export default Header;
